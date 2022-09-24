e443ad7e21e05397866dfebab4cf5baaa44dafbc
niko-2112 <72264523+niko-2112@users.noreply.github.com>
2022-09-24 12:38:17 +0300

    homework01

--- homework/homework01/script.js
+++ homework/homework01/script.js
@@ -1,48 +1,46 @@
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 
-let i = 0;
-let direction = 1;
+let size_x = 10;
+let size_y = 10;
 
-let up_down = 0;
-let k = 0;
-let color = "#ff0000"
+let cur_x = 0;
+let cur_y = 0;
 
-document.addEventListener("keydown", function (event) { // отвечает за передвежение
-    if (event.code === "ArrowRight") {
-        direction = 1;
-        up_down = 0;
-    } else if (event.code === "ArrowLeft") {
-        direction = -1;
-        up_down = 0;
+let speed_x = 5;
+
+let color = "#000000"
+
+// генериреуем случайный цвет
+function generateColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+// Изменяем цвет прямоугольника
+function handler_keydown(event) {
+    // нажатие клавиши "c"
+    if (event.keyCode === 67) {
+        console.log(color)
+        color = generateColor();
+        ctx.fillStyle = color;
     }
+}
 
-    if (event.code === "ArrowDown") {
-        up_down = -1;
-        direction = 0;
-    } else if (event.code === "ArrowUp") {
-        up_down = 1;
-        direction = 0;
+addEventListener("keydown", handler_keydown);
+
+setInterval(function () {
+    drawRectangle(cur_x, cur_y, size_x, size_y)
+    if (cur_x >= canvas.width - size_x) {
+        speed_x = -speed_x;
+    } else if (cur_x < 0) {
+        speed_x = -speed_x;
     }
-    if (event.code === "click") {
-        color = '#' + (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase();
-    }
+    cur_x += speed_x;
 
+}, 50);
 
-});
-
-document.addEventListener("click", function (event) {
-    if (event.which === 1) {
-        color = '#' + (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase();
-    }
-});
-
-setInterval(function () { //отвечает за фигуру и её перемещение
+// перерисовываем пряоугольник
+function drawRectangle(x, y, widthEl, heightEl) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    ctx.fillStyle = color;
-    ctx.fillRect(50 + i, 50 - k, 100, 100);
-    i += direction;
-    k += up_down;
-}, 10);
-
+    ctx.fillRect(x, y, widthEl, heightEl);
+}
